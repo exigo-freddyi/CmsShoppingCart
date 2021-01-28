2bc66ab5dfceae34778d989d965cd05b7bd5734b
Freddy Iñiguez <freddyI@exigo.com>
2021-01-28 09:46:00 -0600

    Editing an existing Page from Admin.

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -33,7 +33,6 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             return View();
         }
 
-
         // POST: Admin/Pages/AddPage
         [HttpPost]
         public ActionResult AddPage(PageVM model)
@@ -88,6 +87,93 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
 
             // Redirect
             return RedirectToAction("AddPage");
+        }
+
+        // GET: Admin/Pages/EditPage/id
+        [HttpGet]
+        public ActionResult EditPage (int id)
+        {
+            // Declare pageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                // Get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                // confirm page exists
+                if (dto == null)
+                {
+                    return Content("The page does not exists");
+                }
+
+                // Init pageVM
+                model = new PageVM(dto);
+            }
+
+            // Return view with model
+            return View(model);
+        }
+
+        // POST: Admin/Pages/EditPage
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            // Checlk model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using(Db db = new Db())
+            {
+                // Get page id
+                int id = model.Id;
+
+                // Init slug
+                string slug = "home";
+
+                // Get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                // DTO the title
+                dto.Title = model.Title;
+
+                // Check for slug and set it of need be
+                if (model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    } 
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+
+                // Make sure title and slug are unique
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title) ||
+                    db.Pages.Where(x => x.Id != id).Any(x => x.Title == slug))
+                {
+                    ModelState.AddModelError("", "Tha ttitle or slug already exists.");
+                    return View(model);
+                }
+
+                // DTO the rest
+                dto.Slug = model.Slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+
+                // Save DTO
+                db.SaveChanges();
+            }
+
+            // Set TempData message
+            TempData["SM"] = "You have edited the page!";
+
+            // Redirectr
+            return RedirectToAction("EditPage");
         }
     }
 }
